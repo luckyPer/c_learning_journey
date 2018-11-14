4a3db79ebc61fee3d254c4c6ca633014a305f859
pan <pzj.lovers@hotmail.com>
2018-11-14 19:08:05 +0800

    .

--- hello_world/hello.c
+++ hello_world/hello.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main()
 {
 
-   printf("Hello, World! \n");
-
-   system("pause");
-   return 0;
+    printf("Hello, World! \n");
+    printf(" size : %lu ", sizeof(int) );
+    system("pause");
+    return 0;
 }
