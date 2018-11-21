9669c165186892295664c36da41858ffcb090517
pan <pzj.lovers@hotmail.com>
2018-11-21 16:43:20 +0800

    ..

--- V1/main.c
+++ V1/main.c
@@ -7,7 +7,23 @@ extern void write_extern();
 int main()
 {
     count = 5;
-    write_extern();
+    //write_extern();
+    getPrimeNumber();
     system("pause");
     return 0;
+}
+
+void getPrimeNumber()
+{
+    int i, j;
+    for (i = 2; i < 100; i++)
+    {
+        for (j = 2; j <= (i / j); j++)
+        {
+            if (!(i % j))
+                break;
+        }
+        if (j > (i / j))
+            printf("Prime Number is %d\n", i);
+    }
 }
--- hello_world/hello.c
+++ hello_world/hello.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+extern int a; //声明变量，未定义，不建立存储空间。 对比 int a
+
+#define LENGTH  10
+
 int main()
 {
-
-    printf("Hello, World! \n");
+    const int WIDTH = 5;
     printf(" size : %lu ", sizeof(int) );
     system("pause");
     return 0;
