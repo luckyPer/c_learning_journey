cddb4c0bba53794167abc65b3da741b9a8cfdf14
pan <pzj.lovers@hotmail.com>
2019-04-14 22:20:03 +0800

    add linklist double

--- DataStructureV1/LinkList/LinkListDouble.c
+++ DataStructureV1/LinkList/LinkListDouble.c
@@ -1,3 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+#define LEN sizeof(struct linkList)
+
+struct linkList
+{
+    int data;
+    struct linkList *next, *prev;   
+};
+
+void AddlinkList(int index)
+{
+    int j = 1;
+    struct linkList *head = (struct linkList*)malloc(LEN);
+    struct linkList *s, *r;
+    s = head->next;
+    while(s && j < index)
+    {
+        j++;
+        s = s->next;
+    }
+    r->next = s->next;
+    s->next->prev = r;
+    r->prev = s;
+    s->next = r;
+}
+
+
+//core code
+void AddlinkList(int index)
+{
+    int j = 1;
+    struct linkList *head = (struct linkList*)malloc(LEN);
+    struct linkList *s, *r = (struct linkList*)malloc(LEN);
+    //a1  ->  a2  ->  a2 
+    //s    <-  r    <-   
+    r = s->next;
+    s->next = r->next;
+    r->next->prev = s;
+    free(r);
+}
