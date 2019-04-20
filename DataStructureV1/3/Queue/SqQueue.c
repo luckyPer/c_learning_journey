7a0a9754fb85c88480343e05a13c4893f3f54392
pan <pzj.lovers@hotmail.com>
2019-04-20 21:04:56 +0800

    re-enginge folder

--- DataStructureV1/3/Queue/SqQueue.c
+++ DataStructureV1/3/Queue/SqQueue.c
@@ -7,7 +7,7 @@
 typedef int ElementType;
 typedef struct SqQueue
 {
-    ElementType Data;
+    ElementType Data[initSize];
     int front, rear;    //记录头指针和尾指针, 其中尾指针尾元素的index + 1的位置, 防止为空的时候front 和rear 在一起
 }SqQueue;   //会造成假溢出, 假溢出是指实际有剩余空间, 但rear指针已经到最后了, 使得无法再push, 是由于顺序队列的结构所造成的
 
