57feff90860e06d40f3f407f23d7bd4bc13872b8
pan <pzj.lovers@hotmail.com>
2019-06-05 21:17:51 +0800

    BTree

--- csbiji_/6/Loop.c
+++ csbiji_/6/Loop.c
@@ -16,7 +16,7 @@ void visit(BTNode *P){}
 
 
 /*
- *@description: 二叉树非递归 先 序遍历.---- 从根节点开始, 入栈一个节点, 不停地执行以下操作: 若栈不为空, 就出栈一个元素,
+ *@description: 二叉树非递归 先 序遍历. 需要一个栈 ---- 从根节点开始, 入栈一个节点, 不停地执行以下操作: 若栈不为空, 就出栈一个元素,
  并且访问该元素, 然后把左右孩子入栈(非空判断, 注意顺序 :右孩子先, 左孩子后)
  *@params1: BtNode : 二叉树
  *@return: Void
@@ -88,7 +88,7 @@ void PostOrderNoneR(BTNode *bt)
 }
 
 /*
- *@description: 中序遍历二叉树 非递归.  从根节点开始 ,沿着左子树路径一直全部入栈. 
+ *@description: 中序遍历二叉树 非递归, 需要一个栈.  从根节点开始 ,沿着左子树路径一直全部入栈. 
  若栈不为空,  则出栈并访问出栈元素, 在指向右孩子. loop
  *@params1: BtNode : 二叉树
  *@return: Void
@@ -114,4 +114,42 @@ void inorderNoneR(BTNode *BT)
             p = p->rchild;
         }      
     }    
+}
+
+/*
+ *@description: 层次遍历二叉树, 需要一个队列. 先从根结点入队, 若队列不为空, 把队列第一个元素 出队, 
+ 左右孩子不为空的话, 左右孩子入队列, loop   
+ *@params1: BtNode : 二叉树
+ *@return: Void
+ *@date: 2019-06-05 19:56:54
+*/
+void level(BTNode *BT)
+{
+    if (BT == NULL)
+    {
+        return;
+    }    
+    BTNode *que[maxsize];
+    BTNode *p = NULL;
+    int rear = 0;
+    int front = 0;
+    
+    rear = (rear + 1) % maxsize;
+    que[rear] = BT;
+    while (front != rear)
+    {
+        front = (front + 1) % maxsize;
+        p = que[front];
+        visit(p);
+        if (p->lchild)
+        {
+            rear = (rear + 1) % maxsize;
+            que[rear] = p->lchild;
+        }
+        if (p->rchild)
+        {
+            rear = (rear + 1) % maxsize;
+            que[rear] = p->rchild;
+        }
+    }
 }
