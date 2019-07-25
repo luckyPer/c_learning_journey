cae6ce4d76af9120143109cf4ddfcbec8857cbc5
pan <pzj.lovers@hotmail.com>
2019-07-25 23:13:16 +0800

    MF , 选择题做得不开心

--- 王道习题/Tree/Tree.c
+++ 王道习题/Tree/Tree.c
@@ -11,7 +11,12 @@ typedef struct
     struct BtNode *rchild;
 }BtNode;
 
-
+typedef struct BSTNode
+{
+    int val;
+    struct BSTNode *lchild;
+    struct BSTNode *rchild;
+}BSTNode;
 /*
  *@description: 判断一课二叉树是否是 平衡二叉树
  时间复杂度 nlogN 
@@ -34,4 +39,41 @@ int getHeight(BtNode *bt)
     int leftHeight = getHeight(bt->lchild);
     int rightHeight = getHeight(bt->rchild);
     return (leftHeight > rightHeight ? leftHeight + 1 : rightHeight +1);
+}
+
+/*
+ *@description: 求出给定二叉排序树的最大值和最小值
+ 最右下即为最大;
+ 最左下即为最小.
+ *@date: 2019-07-25 22:53:13
+*/
+int getMax(BSTNode *bst)
+{
+    while (bst)
+    {
+        bst = bst->lchild;
+    }
+    return bst->val;
+}
+
+/*
+ *@description: 判断二叉树是否为 二叉排序树
+ 递归中序遍历, 使前一个值要比后一个值小
+ *@date: 2019-07-25 22:55:56
+*/
+int pre;//初始为负无穷
+int IsBST(BSTNode *bst)
+{
+    int b1;
+    if (!bst)
+    {
+        return 1;
+    }
+    b1 = IsBST(bst->lchild);
+    if (b1 != 1 || pre >= b1)
+    {
+        return 0;
+    }
+    pre = b1;
+    return IsBST(bst->rchild);
 }
