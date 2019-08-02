3b82ee7b84709848d0cdb6ffc103eb6b1fb59760
pan <pzj.lovers@hotmail.com>
2019-08-02 23:48:50 +0800

    sai

--- wangdao/Seq/LinkNode.c
+++ wangdao/Seq/LinkNode.c
@@ -10,25 +10,43 @@ typedef struct LinkNode
     struct LinkNode *next; 
 }LinkNode;
 
-//
+//1 Recursion
 void DeleteX(LinkNode *L, int X)
 {
     if (L == NULL)
     {
         return;
     }
+    if (L->data == X)
+    {
+        LinkNode *S = L;
+        L = L->next;
+        free(S);
+        DeleteX(L,X); 
+    }   
     else
     {
-        if (L->data == X)
+        DeleteX(L->next,X); 
+    }    
+}
+
+
+//2 None Recursion
+void deleteXNoneRecursion(LinkNode *l, int X)
+{
+    LinkNode *s;
+    LinkNode *p = l;
+    while (p->next!=NULL)
+    {
+        if (p->next->data == X)
         {
-            LinkNode *S = L;
-            L = L->next;
-            free(S);
-            DeleteX(L,X); 
-        }   
+            s = p->next;
+            p->next = s->next;
+            free(s);
+        }
         else
         {
-            DeleteX(L->next,X); 
-        }  
-    }    
+            p = p->next;
+        }        
+    }
 }
