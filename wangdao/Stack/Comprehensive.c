36a4a807484aae6f182fd0d692f3d731530e5243
pan <pzj.lovers@hotmail.com>
2019-08-13 22:15:00 +0800

    ...

--- wangdao/Stack/Comprehensive.c
+++ wangdao/Stack/Comprehensive.c
@@ -11,7 +11,7 @@
 bool judge(char *operation)
 {
     int i = 0;
-    char *stack[maxsize];
+    char stack[maxsize];
     int top = -1;
     while (operation[i] != '\0')
     {
@@ -66,7 +66,7 @@ bool judge(char *operation)
 //2
 void schedu(char *train)
 {
-    char *stack[maxsize];
+    char stack[maxsize];
     int top = -1;
     char s;
     int i = 0;
@@ -99,31 +99,33 @@ int Pn(int X, int n)
     return 2*X*Pn(X, n-1)-2*(n-1)*Pn(X, n-2);
 }
 
-int PnNoneRecursion();
-
-
-//4
-//B 卡车
-//T 货车
-
-void dockSimulate(char *vehicle)
+int PnNoneRecursion(int X, int n)
 {
-    char *stack[maxsize];
-    int top = -1;
-    char s;
-    int i = 0;
-    int k = 0;
-    while (vehicle[i] != '\0')
+    struct 
     {
-        switch (vehicle[i])
-        {
-        case 'H':
-            stack[++top];
-            break;
-        case 'S':
-        vehicle[k++] = vehicle[i];
-            break;
-        }
-        i++;
+        int val;
+        int no;
+    }stk[maxsize];
+
+    int top = -1;
+    int v1 = 1,v2 = 2*X;
+    int i = n;
+    while (i>=2)
+    {
+        stk[++top].no = i;
+        i--;
     }
+    while (top != -1)
+    {
+        stk[top].val = 2*X*v2 - 2*(stk[top].no - 1)*v1;
+        v1 = v2;
+        v2 = stk[top].val;
+        top--;   
+    }
+    if (n==1)
+    {
+        return v1;
+    }
+    
+    return v2;
 }
