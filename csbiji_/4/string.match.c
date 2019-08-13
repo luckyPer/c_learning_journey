a13ff99b5cd0c27bfc5a1f16c5eaa65a39be48c2
pan <pzj.lovers@hotmail.com>
2019-08-13 20:58:57 +0800

    kmp

--- csbiji_/4/string.match.c
+++ csbiji_/4/string.match.c
@@ -56,7 +56,7 @@ int naive(Str str, Str subStr)
 */
 int KMP(Str str, Str subStr, int next[])
 {
-    int i,j = 0;
+    int i,j = 1;
     while (i <= str.len && j <= subStr.len)
     {
         if (j == 0 || str.ch[i] == str.ch[j])
@@ -81,8 +81,11 @@ int KMP(Str str, Str subStr, int next[])
 
 /*
  *@description: 匹配串下标依旧从1开始, 运用已知的 next[k] 求出 next[k+1]
+    j: S(t) 时的状态, 可称为Fr
+    k: S(t+1) 时的状态,可称为Fl
+
     1. str[k] == str[j] 时, next[k+1] = next[k] +1;
-    2. str[k] == str[j] 时, 循环, 让next[k] = k, 使得k=0 或者使得满足1. 为止的时候. k= next[j+1] = 1
+    2. str[k] != str[j] 时, 循环, 让next[k] = k, 使得k=0 或者使得满足1. 为止的时候. k= next[j+1] = 1
     3. next[1] = 0
  *@params1: 匹配串
  *@params2: next数组
@@ -98,13 +101,48 @@ int getNext(Str str, int next[])
     {
         if(k == 0 || str.ch[j] == str.ch[k])
         {
-            next[j+1] = k+1;
+            //next[j+1] = k+1;
             j++;
             k++;
+            next[j] = k;
         }
         else
         {
             k = next[k];
         }     
     }  
+}
+
+/*
+ *@description: 求nextVAL
+ 先求出对应的next数组
+ 1. j = 1 时, nextval[1] = 0 ,作为特殊标记
+ 2. pj != pk 时, nextval[j] = k
+ 3. pj == pk 时, nextval[j] = nextval[k]
+ *@date: 2019-08-13 20:53:24
+*/
+void getnextval(Str str, int nextval[])
+{
+    int j = 1, k = 0;
+    nextval[1]  =0;
+    while (j < str.len)
+    {
+        if(k == 0 || str.ch[j] == str.ch[k])
+        {
+            k++;
+            j++;
+            if (str.ch[j] != str.ch[k])
+            {
+                nextval[j] = k;
+            }
+            else
+            {
+                nextval[j] = nextval[k];
+            }
+        }
+        else
+        {
+            k = nextval[k];
+        }
+    } 
 }
