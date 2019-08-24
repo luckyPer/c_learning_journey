b14833a6567ac8c35ee3e4923cfd4d3b7dac4b10
pan <pzj.lovers@hotmail.com>
2019-08-24 19:21:45 +0800

    m

--- csbiji_/8/sort.c
+++ csbiji_/8/sort.c
@@ -242,3 +242,31 @@ void merge(int arr[], int low, int mid, int high)
         arr[k++] = right[j++];
     }   
 }
+
+
+void HalfInsertSort(int arr[], int n)
+{
+    int i,j,low,high,mid;
+    for ( i = 2; i <= n; i++)
+    {
+        arr[0] = arr[i];
+        low = 1, high = i-1;
+        while (low < high)
+        {
+            mid = (low + high)/2;
+            if (arr[mid] > arr[i])
+            {
+                high = mid -1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        for ( j = i-1; j >= high + 1; j--)
+        {
+            arr[j+1] = arr[j];
+        }
+        arr[high+1] = arr[0];
+    }   
+}
